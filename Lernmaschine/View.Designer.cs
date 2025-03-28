b748de856788892874332e36c393ad226af6b1ad
Toeller <toeller@hhbk.de>
2025-03-28 10:44:05 +0100

    V1.0.7

--- Lernmaschine/View.Designer.cs
+++ Lernmaschine/View.Designer.cs
@@ -336,7 +336,7 @@
             Controls.Add(textBoxVorderseite);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "View";
-            Text = "Lernmaschine V1.0.6";
+            Text = "Lernmaschine V1.0.7";
             FormClosing += View_FormClosing;
             groupBoxModus.ResumeLayout(false);
             groupBoxModus.PerformLayout();
