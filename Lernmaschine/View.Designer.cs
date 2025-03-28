f6e53efa1549aed457ca5acc755cffc66a8367fa
CToellerPriv <christoph.toeller@gmx.de>
2025-03-28 21:12:22 +0100

    1.0.10 mit Titeländerung

--- Lernmaschine/View.Designer.cs
+++ Lernmaschine/View.Designer.cs
@@ -336,7 +336,7 @@
             Controls.Add(textBoxVorderseite);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "View";
-            Text = "Lernmaschine V1.0.9";
+            Text = "Lernmaschine V1.0.10";
             FormClosing += View_FormClosing;
             groupBoxModus.ResumeLayout(false);
             groupBoxModus.PerformLayout();
